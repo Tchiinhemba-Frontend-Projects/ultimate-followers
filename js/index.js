3696c3edd375aafd4a77dbab5833834cdc51d952
eladioclaudio <elladioclaudio@gmail.com>
2022-01-13 09:37:56 +0100

    update

--- js/index.js
+++ js/index.js
@@ -1,25 +1,41 @@
 
 
 const container = document.querySelectorAll('.content')[1];
-const gridElementsList = document.querySelector('.grid').querySelectorAll('.grid__item');
+const grid = document.querySelector('.grid');
+const overlay = document.querySelector('.overlay')
 
 
 axios('https://api.github.com/users/eladioclaudio/followers')
-    .then(response => getDataset(response.data))
+    .then(response => handler(response.data))
     .catch(response => console.log(response));
 
 
+function handler(data) {
 
 
+    data.map((value, index) => {
+        const gridElementsList = grid.querySelectorAll('.grid__item');
+        const overlayItems = overlay.querySelectorAll('.overlay__item')
 
-function getDataset(data) { 
-    handler(gridElementsList, data);
+        if (gridElementsList.length <= data.length) {
+            const elCopy = gridElementsList[index].cloneNode(true);
+            const overCopy = overlayItems[index].cloneNode(true);
+
+            grid.appendChild(elCopy)
+            overlay.appendChild(overCopy)
+        }
+        
+        
+
+        const boxImage = gridElementsList[index].querySelector('.box__img');
+        const name = gridElementsList[index].querySelector('.box__title-inner');
+
+        gridElementsList[index].setAttribute('href', `#preview-${index}`);
+        overlayItems[index].setAttribute('id', `preview-${index}`);
+
+        boxImage.setAttribute('src', value.avatar_url);
+        name.setAttribute('data-hover', value.login)
+        name.remove()
+    })
+
 }
-
-
-function handler(listElements, data) {
-    
-}
-
-
-
